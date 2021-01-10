5cbc05891142d100b0224fc8fab35832dc6bbdc5
Ahmet Emrebas <aemrebasus@gmail.com>
2021-01-10 16:59:02 -0600

    none

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,19 @@
+import {
+  animate,
+  animateChild,
+  group,
+  query,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'client';
