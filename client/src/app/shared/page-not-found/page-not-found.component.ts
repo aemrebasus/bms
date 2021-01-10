ea2380df6255a8d16736749fa0ffc59d853d4321
Ahmet Emrebas <aemrebasus@gmail.com>
2021-01-10 16:09:25 -0600

    Template for managing subscription and unsubscription (rxjs) in components

--- client/src/app/shared/page-not-found/page-not-found.component.ts
+++ client/src/app/shared/page-not-found/page-not-found.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.scss']
+  styleUrls: ['./page-not-found.component.scss'],
 })
-export class PageNotFoundComponent implements OnInit {
+export class PageNotFoundComponent implements OnInit, OnDestroy {
+  status = 404;
+  subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    const routerSubscription = this.activatedRoute.data.subscribe((data) => {
+      this.status = data.status;
+    });
+
+    this.subscriptions.push(routerSubscription);
   }
 
+  ngOnDestroy(): void {
+    for (const subscription of this.subscriptions) {
+      subscription.unsubscribe();
+    }
+  }
 }
